de0d31e8abd5ef884fa934218bcebae78d4ac786
Akinizer <salazarslyterin@hotmail.com>
2020-05-10 10:41:18 +0300

    Pytonzilla

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import requests, bugzilla
+import requests, bugzilla, datetime
 
 # pip install bugzilla
 
@@ -63,20 +63,52 @@ def printReportsDate():
     print(r.text)
 
 
-def search(keyword):
+def searchBugs(keyword, time1, time2):
     url = "https://bugzilla.mozilla.org/rest/"
     b = bugzilla.Bugzilla(url=url,api_key=apikey)
 
-    print()
     # keyword: "eclipse"
     # get list of "bug arrays" from serach
     # check creation_time attribute of bugs in the list
         # if year of creation_time > 210
             # print the bug array
+
+    bugarray = []
     for i in b.quick_search(keyword)['bugs']:
         creationTime = i['creation_time']
-        year = int(creationTime[0:4])
-        if year>2010:
-            print(i)
 
-search("eclipse")
+        year = int(creationTime[0:4])
+        month = int(creationTime[5:7])
+        day = int(creationTime[8:10])
+
+        date = datetime.datetime(year,month,day)
+        if (date-time1).total_seconds()>0 and (time2-date).total_seconds()>0:
+            bugarray.append(i)
+    return bugarray
+
+def printBugs(keyword,time1,time2):
+    # searchBugs gets a keyword to search in query of bugzilla, a start time t1 and a finish time t2,
+    # returns a list of bugs with attributes(labels)
+
+    for i in searchBugs(keyword, t1,t2):
+        print(i)
+
+# show bugs between two dates t1, t2
+
+year1=2010
+month1=10
+day1=10
+
+t1 = datetime.datetime(year1,month1,day1)
+
+year2=2019
+month2=12
+day2=12
+
+t2 = datetime.datetime(year2,month2,day2)
+
+keyword = "bugs"
+
+# print a list of bugs between two times t1 and t2 for a query search with a keyword
+
+printBugs(keyword,t1,t2)
